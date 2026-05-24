53753169369a43aeae1e3dbc65e29d56b350a859
Chakradhar Reddy Yerragudi <chakradharvasurama@gmail.com>
2026-05-24 18:26:19 +0530

    Add: regular-expression-matching - submission-5

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-5.py
@@ -0,0 +1,39 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        n1=len(s)
+        n2=len(p)
+        dp={}
+        dp[(n1,n2)]=True
+
+
+        def backtrack(i,j)->bool:
+
+            if (i,j) in dp:
+                return dp[(i,j)]
+            if j==n2:
+                dp[(i,j)]=False
+                return False
+            if i==n1:
+                if j+1<n2 and p[j+1]=='*':
+                    dp[(i,j)]=backtrack(i,j+2)
+                else:
+                    dp[(i,j)]=False
+                return dp[(i,j)]
+
+
+
+            if s[i]==p[j] or  p[j]=='.':
+                if j+1<n2 and p[j+1]=='*':
+                    dp[(i,j)]= backtrack(i+1,j) or backtrack(i,j+2)
+                else:
+                    dp[(i,j)]= backtrack(i+1,j+1)
+            elif j+1<n2 and p[j+1]=='*':
+                dp[(i,j)]= backtrack(i,j+2)
+
+            else:
+                dp[(i,j)]= False
+            return dp[(i,j)]
+        return backtrack(0,0)
+            
+
+        
