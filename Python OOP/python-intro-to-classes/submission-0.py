37150c36e9412cf560006e9fbdca28374142033f
Chakradhar Reddy Yerragudi <chakradharvasurama@gmail.com>
2026-06-07 23:23:16 +0530

    Add: python-intro-to-classes - submission-0

---
+++ Python OOP/python-intro-to-classes/submission-0.py
@@ -0,0 +1,12 @@
+
+
+
+
+
+# Do not modify below this line
+class Pet:
+    def __init__(self,name,species):
+        self.name=name
+        self.species=species
+my_pet = Pet("Fluffy", "cat")
+print(f"My pet is a {my_pet.species} named {my_pet.name}")
